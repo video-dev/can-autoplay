fe8bab35c24076b175b6d38391e359b456b9a73b
Nicolas Siver <nicolas@siver.im>
2017-12-02 08:27:27 -0500

    change test cases to use new API for return values

--- test/audio.spec.js
+++ test/audio.spec.js
@@ -5,10 +5,16 @@ import { audio } from '../lib/index'
 
 test.serial('returns true for available audio auto-playback', t => {
   mockPlay(() => Promise.resolve())
-  return audio().then(result => t.true(result))
+  return audio().then(({result}) => t.true(result))
 })
 
 test.serial('returns false for unavailable audio auto-playback', t => {
   mockPlay(() => Promise.reject(new Error()))
-  return audio().then(result => t.false(result))
+  return audio().then(({result}) => t.false(result))
+})
+
+test.serial('returns error object', t => {
+  const message = 'Aborted.'
+  mockPlay(() => Promise.reject(new Error(message)))
+  return audio().then(({error}) => t.is(error.message, message))
 })
--- test/video.spec.js
+++ test/video.spec.js
@@ -5,15 +5,35 @@ import { video } from '../lib/index'
 
 test.serial('returns true for available video auto-playback', t => {
   mockPlay(() => Promise.resolve())
-  return video().then(result => t.true(result))
+  return video().then(({result}) => t.true(result))
 })
 
 test.serial('returns false for unavailable video auto-playback', t => {
   mockPlay(() => Promise.reject(new Error()))
-  return video().then(result => t.false(result))
+  return video().then(({result}) => t.false(result))
 })
 
 test.serial('returns true for old browsers', t => {
   mockPlay(() => undefined)
-  return video().then(result => t.true(result))
+  return video().then(({result}) => t.true(result))
+})
+
+test.serial('reaches timeout with a false result', t => {
+  t.plan(2)
+  mockPlay(() => new Promise(resolve => undefined))
+  return video({timeout: 0}).then(({result, error}) => {
+    t.false(result)
+    t.regex(error.message, /timeout/i)
+  })
+})
+
+test.serial('returns error object', t => {
+  const message = 'Aborted.'
+  mockPlay(() => Promise.reject(new Error(message)))
+  return video().then(({error}) => t.is(error.message, message))
+})
+
+test.serial('return null error by default', t => {
+  mockPlay(() => Promise.resolve())
+  return video().then(({error}) => t.is(error, null))
 })
