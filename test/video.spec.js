3684c5673d6f09b42954cfea9dbac68adeaf5964
Caio Gondim <me@caiogondim.com>
2018-02-15 15:53:17 -0500

    Fix tests

--- test/video.spec.js
+++ test/video.spec.js
@@ -1,27 +1,26 @@
 import test from 'ava'
-
 import { mockPlay } from './common'
-import { video } from '../lib/index'
+import canAutoplay from '../lib/index'
 
 test.serial('returns true for available video auto-playback', t => {
   mockPlay(() => Promise.resolve())
-  return video().then(({result}) => t.true(result))
+  return canAutoplay.video().then(({result}) => t.true(result))
 })
 
 test.serial('returns false for unavailable video auto-playback', t => {
   mockPlay(() => Promise.reject(new Error()))
-  return video().then(({result}) => t.false(result))
+  return canAutoplay.video().then(({result}) => t.false(result))
 })
 
 test.serial('returns true for old browsers', t => {
   mockPlay(() => undefined)
-  return video().then(({result}) => t.true(result))
+  return canAutoplay.video().then(({result}) => t.true(result))
 })
 
 test.serial('reaches timeout with a false result', t => {
   t.plan(2)
   mockPlay(() => new Promise(resolve => undefined))
-  return video({timeout: 0}).then(({result, error}) => {
+  return canAutoplay.video({timeout: 0}).then(({result, error}) => {
     t.false(result)
     t.regex(error.message, /timeout/i)
   })
@@ -30,10 +29,10 @@ test.serial('reaches timeout with a false result', t => {
 test.serial('returns error object', t => {
   const message = 'Aborted.'
   mockPlay(() => Promise.reject(new Error(message)))
-  return video().then(({error}) => t.is(error.message, message))
+  return canAutoplay.video().then(({error}) => t.is(error.message, message))
 })
 
 test.serial('return null error by default', t => {
   mockPlay(() => Promise.resolve())
-  return video().then(({error}) => t.is(error, null))
+  return canAutoplay.video().then(({error}) => t.is(error, null))
 })
